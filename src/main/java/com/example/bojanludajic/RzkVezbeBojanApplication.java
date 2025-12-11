e767067209e560ceb500dedc06bed5805ef823ac
Bojan Ludajic <bojan.ludajic@gmail.com>
2025-12-11 13:32:20 +0100

    first commit

---
+++ src/main/java/com/example/bojanludajic/RzkVezbeBojanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bojanludajic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RzkVezbeBojanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RzkVezbeBojanApplication.class, args);
+    }
+
+}
